2a1241b8346f0764d5ce665a287c1f622d9656f3
Muaz Hussain <muaz.self@gmail.com>
2024-11-03 20:28:54 +0600

    Add JWT strategy and guard
    - Implemented JWT authentication strategy for secure access
    - Added JWT guard to protect routes and ensure authenticated access

---
+++ backend/judgebox-backend/src/users/strategies/jwt.strategy.ts
@@ -0,0 +1,35 @@
+import {
+    ExtractJwt,
+    Strategy
+} from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import {
+    Injectable,
+    UnauthorizedException
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { UsersEntity } from '../entities/users.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectRepository(UsersEntity) private readonly userRepository: Repository<UsersEntity>,
+    ) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: 'SECRET',
+            ignoreExpiration: false,
+            passReqToCallback: true,
+        });
+    }
+
+    async validate(request: Request, payload: any): Promise<string> {
+        const { email } = payload;
+        const user = await this.userRepository.findOne({ where: { email } });
+        if (!user) {
+            throw new UnauthorizedException();
+        }
+        return user.id;
+    }
+}
