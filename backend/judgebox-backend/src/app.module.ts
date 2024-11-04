7dc1c84b37997342353ae9939f9ca7dc3f32d1eb
Muaz Hussain <muaz.self@gmail.com>
2024-11-04 22:52:21 +0600

    Add database configurations
    - Configured database connection settings

--- backend/judgebox-backend/src/app.module.ts
+++ backend/judgebox-backend/src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'judgebox',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
