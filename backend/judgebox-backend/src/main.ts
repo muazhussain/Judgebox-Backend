145c401b5c7070a6d8c624b2a0962d61b1b51373
Muaz Hussain <muaz.self@gmail.com>
2024-11-04 22:56:19 +0600

    Update main.ts with basic setup and configurations
    - Added initial setup and configuration to main.ts

--- backend/judgebox-backend/src/main.ts
+++ backend/judgebox-backend/src/main.ts
@@ -1,8 +1,50 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import { ValidationPipe } from '@nestjs/common';
+import {
+  DocumentBuilder,
+  SwaggerModule
+} from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet.default());
+
+  app.enableCors();
+
+  app.use(
+    rateLimit.default({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+      standardHeaders: true,
+      legacyHeaders: false,
+    }),
+  );
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    transformOptions: { enableImplicitConversion: true },
+    whitelist: true,
+  }));
+
+  const config = new DocumentBuilder()
+    .setTitle('JudgeBox API')
+    .setDescription('The JudgeBox API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      docExpansion: 'none',
+      tagsSorter: 'alpha',
+      operationsSorter: 'alpha',
+      caches: 'no-cache',
+    },
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
