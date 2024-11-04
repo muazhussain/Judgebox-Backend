afd7c726ba91d932950db1c9f79b0d1c78372ddb
Muaz Hussain <muaz.self@gmail.com>
2024-11-04 22:51:00 +0600

    Implement auth controller
    - Added API endpoint for user registration
    - Added API endpoint for user login
    - Added API endpoint for user logout

--- backend/judgebox-backend/src/users/controllers/auth.controller.ts
+++ backend/judgebox-backend/src/users/controllers/auth.controller.ts
@@ -1,4 +1,64 @@
-import { Controller } from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Post,
+    Res,
+    UseGuards
+} from "@nestjs/common";
+import { UsersService } from "../services/users.service";
+import { ApiTags } from "@nestjs/swagger";
+import { RegisterDto } from "../dtos/register.dto";
+import { LoginDto } from "../dtos/login.dto";
+import { commonResponse } from "src/utils/common-response";
+import { Response } from 'express';
+import { JwtGuard } from "../guards/jwt.guard";
 
+@ApiTags('Auth')
 @Controller('auth')
-export class AuthController { }
+export class AuthController {
+    constructor(
+        private readonly usersService: UsersService,
+    ) { }
+
+    @Post('register')
+    async register(@Body() registerDto: RegisterDto): Promise<any> {
+        try {
+            const data = await this.usersService.register(registerDto);
+            return commonResponse(true, 'User registered successfully', data);
+        } catch (error) {
+            return commonResponse(false, 'User registration failed', error);
+        }
+    }
+
+    @Post('login')
+    async login(@Body() loginDto: LoginDto, @Res({ passthrough: true }) response: Response): Promise<any> {
+        try {
+            const { accessToken, refreshToken, csrfToken } = await this.usersService.login(loginDto);
+            // response.cookie('csrf_token', csrfToken, {
+            //     httpOnly: false,
+            //     secure: true,
+            //     sameSite: 'strict',
+            // });
+            // response.cookie('refresh_token', refreshToken, {
+            //     httpOnly: true,
+            //     secure: true,
+            //     sameSite: 'strict',
+            // });
+            return commonResponse(true, 'User logged in successfully', accessToken);
+        } catch (error) {
+            return commonResponse(false, 'User login failed', error);
+        }
+    }
+
+    @Post('logout')
+    @UseGuards(JwtGuard)
+    async logout(@Res({ passthrough: true }) response: Response) {
+        try {
+            response.clearCookie('csrf_token');
+            response.clearCookie('refresh_token');
+            return commonResponse(true, 'User logged out successfully');
+        } catch (error) {
+            return commonResponse(false, 'User logout failed', error);
+        }
+    }
+}
