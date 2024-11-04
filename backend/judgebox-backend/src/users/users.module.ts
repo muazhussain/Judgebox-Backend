244ad07bf8cdaa6a82d6bbad279b3f599a426486
Muaz Hussain <muaz.self@gmail.com>
2024-11-04 22:57:57 +0600

    Update app module with JWT and Passport dependencies
    - Added JWT and Passport-related dependencies for authentication

--- backend/judgebox-backend/src/users/users.module.ts
+++ backend/judgebox-backend/src/users/users.module.ts
@@ -3,16 +3,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersEntity } from './entities/users.entity';
 import { UsersService } from './services/users.service';
 import { AuthController } from './controllers/auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { UsersController } from './controllers/users.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       UsersEntity,
     ]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'SECRET',
+      signOptions: { expiresIn: '1d' },
+    }),
   ],
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    JwtStrategy,
+  ],
   controllers: [
     AuthController,
+    UsersController
   ]
 })
 export class UsersModule { }
