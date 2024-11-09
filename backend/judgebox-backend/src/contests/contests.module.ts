246fb8597407e9c769d1455903e85290f0740862
Muaz Hussain <muaz.self@gmail.com>
2024-11-09 18:04:24 +0600

    Create and implement contest-registration service
    - Implemented the contest-registration service for handling user registrations in contests

--- backend/judgebox-backend/src/contests/contests.module.ts
+++ backend/judgebox-backend/src/contests/contests.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContestsService } from './services/contests.service';
+import { ContestsRegistrationsService } from './services/contests-registrations.service';
 
 @Module({
   controllers: [],
   providers: [
     ContestsService,
+    ContestsRegistrationsService,
   ]
 })
 export class ContestsModule { }
---
+++ backend/judgebox-backend/src/contests/services/contests-registrations.service.ts
@@ -0,0 +1,94 @@
+import {
+    Injectable,
+    NotFoundException
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ContestRegistrationsEntity } from '../entities/contest-registrations.entity';
+import {
+    DataSource,
+    Repository
+} from 'typeorm';
+import { GetContestRegistrationsDto } from '../dtos/get-contest-registrations.dto';
+
+@Injectable()
+export class ContestsRegistrationsService {
+    constructor(
+        @InjectRepository(ContestRegistrationsEntity) private readonly contestsRegistrationsRepository: Repository<ContestRegistrationsEntity>,
+        private dataSource: DataSource,
+    ) { }
+
+    async createContestRegistration(createContestRegistrationDto: ContestRegistrationsEntity) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const contestRegistration = await queryRunner.manager.save(this.contestsRegistrationsRepository.metadata.target, createContestRegistrationDto);
+            await queryRunner.commitTransaction();
+            await queryRunner.release();
+            return contestRegistration;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getContestRegistrations(contestRegistrationId: string): Promise<ContestRegistrationsEntity> {
+        try {
+            const findContestRegistration = await this.contestsRegistrationsRepository.findOne({
+                select: ['id', 'participant', 'contest'],
+                where: {
+                    id: contestRegistrationId,
+                },
+            });
+            if (!findContestRegistration) {
+                throw new NotFoundException('Contest registration not found');
+            }
+            return findContestRegistration;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getAllContestRegistrations(getContestRegistrationsDto: GetContestRegistrationsDto): Promise<ContestRegistrationsEntity[]> {
+        try {
+            return await this.contestsRegistrationsRepository.find({
+                select: ['id', 'participant', 'contest'],
+                take: getContestRegistrationsDto?.limit || 10,
+                skip: (getContestRegistrationsDto?.page || 1 - 1) * getContestRegistrationsDto?.limit || 10,
+                order: {
+                    createdAt: 'DESC',
+                },
+                where: {
+                    contest: {
+                        id: getContestRegistrationsDto?.contest,
+                    },
+                    participant: {
+                        id: getContestRegistrationsDto?.participant,
+                    },
+                },
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteContestRegistration(contestRegistrationId: string) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const findContestRegistration = await this.contestsRegistrationsRepository.findOne({
+                where: {
+                    id: contestRegistrationId,
+                },
+            });
+            if (!findContestRegistration) {
+                throw new NotFoundException('Contest registration not found');
+            }
+            await queryRunner.manager.softDelete(this.contestsRegistrationsRepository.metadata.target, findContestRegistration);
+            await queryRunner.commitTransaction();
+            await queryRunner.release();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
