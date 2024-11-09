73b642ab4307f7801a0d56782a27371334fe6f54
Muaz Hussain <muaz.self@gmail.com>
2024-11-10 00:21:35 +0600

    Create and implement Problem service
    - Implemented problem service to handle problem-related operations

--- backend/judgebox-backend/src/problems/problems.module.ts
+++ backend/judgebox-backend/src/problems/problems.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ProblemsController } from './controllers/problems.controller';
+import { ProblemsService } from './services/problems.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProblemsEntity } from './entities/problems.entity';
 
 @Module({
-  controllers: [ProblemsController]
+  imports: [
+    TypeOrmModule.forFeature([ProblemsEntity])
+  ],
+  controllers: [ProblemsController],
+  providers: [ProblemsService]
 })
-export class ProblemsModule {}
+export class ProblemsModule { }
---
+++ backend/judgebox-backend/src/problems/services/problems.service.ts
@@ -0,0 +1,110 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { ProblemsEntity } from '../entities/problems.entity';
+import { CreateProblemDto } from '../dtos/create-problem.dto';
+import { GetProblemsDto } from '../dtos/get-problems.dto';
+import { UpdateProblemDto } from '../dtos/update-problem.dto';
+
+@Injectable()
+export class ProblemsService {
+    constructor(
+        @InjectRepository(ProblemsEntity) private readonly problemsRepository: Repository<ProblemsEntity>,
+        private dataSource: DataSource,
+    ) { }
+
+    async createProblem(createProblemDto: CreateProblemDto) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const problem = await queryRunner.manager.save(this.problemsRepository.metadata.target, createProblemDto);
+            await queryRunner.commitTransaction();
+            await queryRunner.release();
+            return problem;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getProblem(problemId: string) {
+        try {
+            const findProblem = await this.problemsRepository.findOne({
+                where: {
+                    id: problemId,
+                },
+            });
+            if (!findProblem) {
+                throw new NotFoundException('Problem not found');
+            }
+            return findProblem;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async getProblems(getProblemsDto: GetProblemsDto) {
+        try {
+            const problems = await this.problemsRepository.find({
+                order: {
+                    createdAt: 'DESC',
+                },
+                where: {
+                    createdBy: {
+                        id: getProblemsDto.createdBy,
+                    },
+                },
+                take: getProblemsDto.limit,
+                skip: (getProblemsDto.page - 1) * getProblemsDto.limit,
+                relations: ['createdBy'],
+            });
+            return problems;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async updateProblem(problemId: string, updateProblemDto: UpdateProblemDto) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const findProblem = await this.problemsRepository.findOne({
+                where: {
+                    id: problemId,
+                },
+            });
+            if (!findProblem) {
+                throw new NotFoundException('Problem not found');
+            }
+            Object.assign(findProblem, updateProblemDto);
+            await queryRunner.manager.update(this.problemsRepository.metadata.target, { id: problemId }, findProblem);
+            await queryRunner.commitTransaction();
+            await queryRunner.release();
+            return findProblem;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async deleteProblem(problemId: string) {
+        const queryRunner = this.dataSource.createQueryRunner();
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try {
+            const findProblem = await this.problemsRepository.findOne({
+                where: {
+                    id: problemId,
+                },
+            });
+            if (!findProblem) {
+                throw new NotFoundException('Problem not found');
+            }
+            await queryRunner.manager.softDelete(this.problemsRepository.metadata.target, { id: problemId });
+            await queryRunner.commitTransaction();
+            await queryRunner.release();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
