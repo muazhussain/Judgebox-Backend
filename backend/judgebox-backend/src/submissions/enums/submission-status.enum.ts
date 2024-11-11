c87b7478f80eaa637ab3b28fa76d5a96daf1102b
Muaz Hussain <muaz.self@gmail.com>
2024-11-11 23:34:58 +0600

    Add submission status enum
    - Defined an enum for submission statuses

---
+++ backend/judgebox-backend/src/submissions/enums/submission-status.enum.ts
@@ -0,0 +1,8 @@
+export enum SubmissionStatus {
+    PENDING = 'pending',
+    IN_QUEUE = 'in_queue',
+    COMPILING = 'compiling',
+    RUNNING = 'running',
+    COMPLETED = 'completed',
+    ERROR = 'error'
+}
