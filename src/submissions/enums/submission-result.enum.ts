925ea498355ccacfcf51ba77cbfc96cad6a44569
Muaz Hussain <muaz.self@gmail.com>
2024-11-12 00:13:49 +0600

    Project restructured

---
+++ src/submissions/enums/submission-result.enum.ts
@@ -0,0 +1,8 @@
+export enum SubmissionResult {
+    ACCEPTED = 'accepted',
+    WRONG_ANSWER = 'wrong_answer',
+    TIME_LIMIT_EXCEEDED = 'time_limit_exceeded',
+    MEMORY_LIMIT_EXCEEDED = 'memory_limit_exceeded',
+    RUNTIME_ERROR = 'runtime_error',
+    COMPILATION_ERROR = 'compilation_error'
+}
